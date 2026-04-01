4ab00eba6a679ad067a2bdb92b0d3319fed56507
AxelAlejandroReséndizÁvila <124049803@upq.edu.mx>
2026-03-31 18:00:45 -0600

    external controller para poblar

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000/api";
+const API_URL = "https://awos-tienda.onrender.com/api" || "http://localhost:4000/api";
 
 export const api = {
   get: async (endpoint) => {
